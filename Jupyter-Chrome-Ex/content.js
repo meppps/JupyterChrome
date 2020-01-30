e9a057f4c25cca93481fbe1fec399e20fbfac519
meppps <55078570+meppps@users.noreply.github.com>
2020-01-30 13:48:27 -0800

    Add files via upload

---
+++ Jupyter-Chrome-Ex/content.js
@@ -0,0 +1,6 @@
+
+// Make jupyter stop killing your eyes
+document.querySelector('#header').style.background = 'cornsilk';
+document.querySelector('#notebook').style.background = 'cadetblue';
+document.querySelector('#notebook-container').style.background = 'cornsilk';
+
