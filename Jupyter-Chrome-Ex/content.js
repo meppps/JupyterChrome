a21e26b5cdb46de6ab97601ee11a876909c802ed
Michael Epps <meppscomplex@gmail.com>
2020-08-17 14:56:57 -0700

    Boilerplate for customizer

--- Jupyter-Chrome-Ex/content.js
+++ Jupyter-Chrome-Ex/content.js
@@ -1,6 +1,44 @@
 
 // Make jupyter stop killing your eyes
-document.querySelector('#header').style.background = 'cornsilk';
-document.querySelector('#notebook').style.background = 'cadetblue';
-document.querySelector('#notebook-container').style.background = 'cornsilk';
+
+if(window.location.href.includes('localhost')){
+    
+    console.log('%c Local Host', 'color: yellow');
+
+    var customizeBtn = document.createElement('button');
+    customizeBtn.id = 'customize';
+    customizeBtn.innerText = 'Customize';
+    document.querySelector('div#header-container').appendChild(customizeBtn);-
+    var customizeArea = document.createElement('div');
+    customizeArea.classList.add('container');
+    customizeArea.classList.add('customize-area');
+    customizeArea.style.cssText = `display:flex;align-items:center`;
+
+    customizeArea.innerHTML = `
+    <h4 class="customH4">Front Layer Color:</h4><input id="frontLayerColor" name="Color Picker" type="color"/>
+    <h4 class="customH4">Background Color:</h4><input id="backgroundColor" name="Color Picker" type="color"/>
+    <h4 class="customH4">Text Color:</h4><input id="textColor" name="Color Picker" type="color"/>
+    `;
+
+
+
+    customizeBtn.onclick = function(){
+        document.querySelector('#header').style.height = '235px';
+        document.querySelector('#header').appendChild(customizeArea);
+
+        // document.querySelectorAll('input[type=color]').forEach((input)=>{
+        //     input.addEventListener('input', updateColor, false);
+        // })
+    }
+    // document.
+
+
+    // document.querySelector('#header').style.background = 'cornsilk';
+    // document.querySelector('#notebook').style.background = 'cadetblue';
+    // document.querySelector('#notebook-container').style.background = 'cornsilk';
+
+}else{
+    console.log('');
+}
+
 
