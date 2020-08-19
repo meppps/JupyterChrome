2e1da5bdc5f21b4745b8a01765bac8efd5a43146
Mikey Epps <55078570+meppps@users.noreply.github.com>
2020-08-18 18:34:20 -0700

    Live updating customizer

--- Jupyter-Chrome-Ex/content.js
+++ Jupyter-Chrome-Ex/content.js
@@ -8,7 +8,7 @@ if(window.location.href.includes('localhost')){
     var customizeBtn = document.createElement('button');
     customizeBtn.id = 'customize';
     customizeBtn.innerText = 'Customize';
-    document.querySelector('div#header-container').appendChild(customizeBtn);-
+    document.querySelector('div#header-container').appendChild(customizeBtn);
     var customizeArea = document.createElement('div');
     customizeArea.classList.add('container');
     customizeArea.classList.add('customize-area');
@@ -20,15 +20,33 @@ if(window.location.href.includes('localhost')){
     <h4 class="customH4">Text Color:</h4><input id="textColor" name="Color Picker" type="color"/>
     `;
 
+    // function FrontLayer(elements)
+    const colorAssignments = {
+        frontLayerColor: ['div#header', 'div#notebook-container'],
+        backgroundColor: ['div.CodeMirror-lines', 'div#notebook']
+    }
 
+    function updateColor(event){
+        var header = document.querySelector('div#header');
+        // console.log(event.target.id);
+        var id = event.target.id;
+        var elementList = colorAssignments[id];
+        var colorHex = event.target.value;
+        elementList.forEach((el)=>{
+            document.querySelectorAll(el).forEach((subEl)=>{
+                subEl.style.backgroundColor = event.target.value;
+            })
+        })
+        // header.style.backgroundColor = event.target.value;
+    }
 
     customizeBtn.onclick = function(){
         document.querySelector('#header').style.height = '235px';
         document.querySelector('#header').appendChild(customizeArea);
 
-        // document.querySelectorAll('input[type=color]').forEach((input)=>{
-        //     input.addEventListener('input', updateColor, false);
-        // })
+        document.querySelectorAll('input[type=color]').forEach((input)=>{
+            input.addEventListener('input', updateColor, false);
+        })
     }
     // document.
 
